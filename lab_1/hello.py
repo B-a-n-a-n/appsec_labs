de525dd1a5c3d03223b4a0a6f02c81712647bb92
Andrew <matveichev.ap@phystech.edu>
2026-04-22 20:36:44 +0300

    Добавили комментарии в hello.py

--- lab_1/hello.py
+++ lab_1/hello.py
@@ -1,6 +1,8 @@
 import typer
 
+# main program loop
 def main(
+    # used parameters
     name: str,
     lastname: str = typer.Option("", help="Фамилия пользователя."),
     formal: bool = typer.Option(False, "--formal", "-f", help="Использовать формальное приветствие."),
@@ -8,9 +10,12 @@ def main(
     """
     Говорит "Привет" пользователю, опционально используя фамилию и формальный стиль.
     """
+    # Check if formal greeting is needed
     if formal:
+        # Give greetings with name and surname
         print(f"Добрый день, {name} {lastname}!")
     else:
+        # Give greetings with name
         print(f"Привет, {name}!")
 
 if __name__ == "__main__":
